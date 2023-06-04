b9e379c04a079b5f69b10a0390ffd4afaa5b060b
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2023-06-04 09:05:59 +0700

    Update Hotel Elephant.lua

--- Hotel Elephant.lua
+++ Hotel Elephant.lua
@@ -9,7 +9,7 @@ local Tab = Window:NewTab("MAIN MENU")
 
 local Toggle = Tab:NewSection("Toggle UI")
 
-Toggle:NewKeybind("Toggle UI", "Click to change keybinds", Enum.KeyCode.LeftAlt, function()
+Toggle:NewKeybind("Toggle UI", "Click to change keybinds", Enum.KeyCode.Delete, function()
 	Library:ToggleUI()
 end)
 
@@ -81,17 +81,12 @@ local Credit = Tab:NewSection("Credits!!")
 
 Credit:NewLabel("UI Library: Kavo")
 Credit:NewLabel("Script By Kob")
-Credit:NewLabel("Some idea is from Moonlight Client Script Hub")
+Credit:NewLabel("Some function is from Moonlight Client Script Hub")
 
 -- Hotel Elephant
 
 local Tab = Window:NewTab("Hotel Elephant")
 local Hotel = Tab:NewSection("Hotel Elephant (Click to execute)")
-
-
-Hotel:NewButton("Get Ulimate money", "Click to execute", function()
-    game.ReplicatedStorage.MoneyRequest:FireServer(false, 2e9, "Cash")
-end)
 
 Hotel:NewTextBox("Set everyone's cash", "set everybody's money", function(txt)
 	local SetMoney = (txt)
@@ -108,4 +103,24 @@ Hotel:NewTextBox("Set everyone's cash", "set everybody's money", function(txt)
 	 task.wait(0.10)
 	    game:GetService("ReplicatedStorage").MoneyRequest:FireServer(unpack(SixNine))
  end
+end)
+
+Hotel:NewButton("Get Ulimate money", "Click to execute", function()
+    game.ReplicatedStorage.MoneyRequest:FireServer(false, 2e9, "Cash")
+end)
+
+Hotel:NewButton("Get 400 Cash", "Click to execute", function()
+    game.ReplicatedStorage.MoneyRequest:FireServer(false, 400, "Cash")
+end)
+
+Hotel:NewButton("TP to HeliSpawn", "Click to execute", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(41.1999969, 97.2000046, 6.4000001, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+end)
+
+Hotel:NewButton("TP to main Island", "Click to execute", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(105, 100, -14, 1, -3.07241316e-11, -2.64707706e-15, 3.07241316e-11, 1, 3.57337377e-08, 2.64597931e-15, -3.57337377e-08, 1)
+end)
+
+Hotel:NewButton("TP to Clothing Island", "Click to execute", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(342, 12, -194, 1, 7.20955962e-09, -5.19142238e-13, -7.20955962e-09, 1, -3.0910885e-09, 5.19119958e-13, 3.0910885e-09, 1)
 end)
