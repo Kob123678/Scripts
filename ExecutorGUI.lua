040189c5a486a9cf28c26090bef6bd430e23841f
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2023-12-05 13:33:18 +0700

    Update ExecutorGUI.lua

--- ExecutorGUI.lua
+++ ExecutorGUI.lua
@@ -98,7 +98,7 @@ UICorner_5.Parent = Clear
 
 -- Scripts:
 
-local function FXKHWS_fake_script() -- TextBox.LocalScript 
+local function JLONCKT_fake_script() -- TextBox.LocalScript 
 	local script = Instance.new('LocalScript', TextBox)
 
 	local TextBox = script.Parent -- Replace with the actual TextBox
@@ -110,8 +110,8 @@ local function FXKHWS_fake_script() -- TextBox.LocalScript
 	end)
 	
 end
-coroutine.wrap(FXKHWS_fake_script)()
-local function FWKGIAL_fake_script() -- Execute.LocalScript 
+coroutine.wrap(JLONCKT_fake_script)()
+local function WVVRO_fake_script() -- Execute.LocalScript 
 	local script = Instance.new('LocalScript', Execute)
 
 	local button = script.Parent
@@ -120,8 +120,8 @@ local function FWKGIAL_fake_script() -- Execute.LocalScript
 		loadstring(textbox.Text)() -- Execute Script From TextBox
 	end)
 end
-coroutine.wrap(FWKGIAL_fake_script)()
-local function YPQG_fake_script() -- Clear.LocalScript 
+coroutine.wrap(WVVRO_fake_script)()
+local function DDKKJ_fake_script() -- Clear.LocalScript 
 	local script = Instance.new('LocalScript', Clear)
 
 	local button = script.Parent
@@ -130,11 +130,56 @@ local function YPQG_fake_script() -- Clear.LocalScript
 		textbox.Text = "" -- Make Textbox Empty
 	end)
 end
-coroutine.wrap(YPQG_fake_script)()
-local function FUWUX_fake_script() -- Main.LocalScript 
+coroutine.wrap(DDKKJ_fake_script)()
+local function WSIFK_fake_script() -- Main.LocalScript 
 	local script = Instance.new('LocalScript', Main)
 
-	script.Parent.Draggable = true
-	script.Parent.Active = true
+	-- Reference to the GUI element
+	local gui = script.Parent
+	
+	-- Variables to store mouse information
+	local dragging = false
+	local offset = Vector2.new(0, 0)
+	
+	-- Function to handle mouse button press
+	local function onMouseButton1Down()
+		dragging = true
+		offset = gui.Position - UDim2.new(0, game:GetService("GuiService"):GetMousePosition().X, 0, game:GetService("GuiService"):GetMousePosition().Y)
+	end
+	
+	-- Function to handle mouse movement
+	local function onMouseMove()
+		if dragging then
+			local newPosition = UDim2.new(0, game:GetService("GuiService"):GetMousePosition().X + offset.X, 0, game:GetService("GuiService"):GetMousePosition().Y + offset.Y)
+			gui.Position = newPosition
+		end
+	end
+	
+	-- Function to handle mouse button release
+	local function onMouseButton1Up()
+		dragging = false
+	end
+	
+	-- Connect functions to events
+	gui.InputBegan:Connect(function(input, gameProcessedEvent)
+		if not gameProcessedEvent then
+			if input.UserInputType == Enum.UserInputType.MouseButton1 then
+				onMouseButton1Down()
+			end
+		end
+	end)
+	
+	game:GetService("UserInputService").InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement then
+			onMouseMove()
+		end
+	end)
+	
+	game:GetService("UserInputService").InputEnded:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			onMouseButton1Up()
+		end
+	end)
+	
 end
-coroutine.wrap(FUWUX_fake_script)()
+coroutine.wrap(WSIFK_fake_script)()
