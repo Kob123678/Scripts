2d47cc490f725ce79b0479dcac2c8922d24c43bf
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-05-11 19:58:59 +0700

    Update babft.lua

--- babft.lua
+++ babft.lua
@@ -10,6 +10,14 @@ local function notify(message)
     })
 end
 
+-- Anti-Afk
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 while true do
     -- Change gravity to 0
     game.Workspace.Gravity = 0
