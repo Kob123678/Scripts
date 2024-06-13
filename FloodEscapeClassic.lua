308bfe2654053b02123e30a6619efa11f10ab072
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-13 18:40:58 +0700

    Update FloodEscapeClassic.lua

--- FloodEscapeClassic.lua
+++ FloodEscapeClassic.lua
@@ -4,7 +4,7 @@ local Window = OrionLib:MakeWindow({Name = "Flood Escape"})
 local Tab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998"})
 local Section = Tab:AddSection({Name = "Auto Farm by Tomato Hub"})
 
-for _, v in ipairs({"Auto Farm", "Debug (Open Console)"}) do
+for _, v in ipairs({"Auto Farm", "Notifications"}) do
     Tab:AddToggle({
         Name = v,
         Default = false,
@@ -14,13 +14,10 @@ for _, v in ipairs({"Auto Farm", "Debug (Open Console)"}) do
     })
 end
 
-getgenv().autofarm, getgenv().debug = false, false
+getgenv().autofarm, getgenv().notifications = false, false
 
 local LP, RunService, wait = game:GetService("Players").LocalPlayer, game:GetService("RunService"), task.wait
-local lastdebug, winsGained, gameModes = tick(), 0, {"Easy", "Medium", "Hard"}
-
-local function debugprint(...) if getgenv().debug then warn(..., tick() - lastdebug) end lastdebug = tick() wait() end
-debugprint('Game Loaded')
+local winsGained, gameModes = 0, {"Easy", "Medium", "Hard"}
 
 local function getThing(mode, thing)
     local mode = workspace[mode]
@@ -32,20 +29,28 @@ end
 local function getRoot() return (LP.Character or LP.CharacterAdded:Wait()) and LP.Character:WaitForChild("HumanoidRootPart") end
 local function touchPart(part) local root = getRoot() firetouchinterest(root, part, 0) firetouchinterest(root, part, 1) end
 
+local function notify(content)
+    if getgenv().notifications then
+        OrionLib:MakeNotification({
+            Name = "Notification!",
+            Content = content,
+            Image = "rbxassetid://4483345998",
+            Time = 5
+        })
+    end
+end
+
 local function playGame(mode)
-    debugprint('Entering Lift..')
     touchPart(getThing(mode, 'entry'))
-    debugprint('Entered Lift')
 
     repeat wait() until getThing(mode, 'info').Value == "Game is Ready!"
-    debugprint('"Game is Ready!" detected.')
-    
+
     for _ = 1, 3 do getThing(mode, 'info').Changed:wait() end
-    debugprint('Touching Exit..')
 
     repeat wait() touchPart(getThing(mode, 'exit')) until LP.Ingame.Value == 0
     winsGained += 1
-    debugprint('Game Won.')
+
+    notify("Gained " .. winsGained .. " wins so far!")
 end
 
 while wait() do
@@ -54,7 +59,6 @@ while wait() do
             local infoval = getThing(mode, 'info').Value
             if infoval == "Game is Ready!" or infoval:lower():match('intermission') then
                 playGame(mode)
-                print("Gained", winsGained, "wins so far!")
             end
         end
     end
