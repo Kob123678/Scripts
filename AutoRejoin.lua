1d73e037a1987dc85ceb96cfa95f0410988bc4cd
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-19 06:52:10 +0700

    Create AutoRejoin.lua

---
+++ AutoRejoin.lua
@@ -0,0 +1,8 @@
+local GuiService = game:GetService("GuiService")
+
+GuiService.ErrorMessageChanged:Connect(function()
+    wait(0.1)
+    local rejoinCode = [[game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)]]
+    syn.queue_on_teleport(rejoinCode)
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+end)
