727bdc768541146b3c8a225ae62089134bf3bd31
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-22 19:49:41 +0700

    Update Anti-Afk.lua

--- Anti-Afk.lua
+++ Anti-Afk.lua
@@ -1,7 +1,10 @@
-game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "Anti-Afk Enabled!", Duration = 5})
-
-local vu = game:service('VirtualUser')
-game.Players.LocalPlayer.Idled:connect(function()
-    vu:CaptureController()
-    vu:ClickButton2(Vector2.new())
-end)
+if _G.ScriptExecuted then
+    game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "You already executed!", Duration = 5})
+else
+    _G.ScriptExecuted = true
+    game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "Anti-Afk Enabled!", Duration = 5})
+    game.Players.LocalPlayer.Idled:connect(function()
+        game:service('VirtualUser'):CaptureController()
+        game:service('VirtualUser'):ClickButton2(Vector2.new())
+    end)
+end
