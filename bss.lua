74060e5ed9d0b98ec11652ccbe867eb04ff6b9e4
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-23 19:25:45 +0700

    Update bss.lua

--- bss.lua
+++ bss.lua
@@ -2,5 +2,4 @@ _G.Autojoin = false -- Set to true to auto join Bee Swarm Simulator
 _G.AntiAfk = true -- Set to true to enable Anti-Afk
 _G.AutoRemovePortal = true -- Set to true to auto remove the retro portal
 _G.AutoRejoin = true -- Set to true to enable Auto Rejoin when kicked
-_G.AutoExecute = false -- Set to true to auto execute when server hop/rejoin
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Kob123678/Scripts/main/scriptbss.lua"))()
