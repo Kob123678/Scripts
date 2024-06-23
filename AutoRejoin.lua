266cdc906cb36bcd8e84539fd8b1f3e25b3acffa
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-23 21:20:56 +0700

    Update AutoRejoin.lua

--- AutoRejoin.lua
+++ AutoRejoin.lua
@@ -1,7 +1,8 @@
+-- Made By SecondKob
 -- Work on auto exec
 if not _G.AutoRejoin then
     _G.AutoRejoin = true
-    game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "Anti-Afk Enabled!", Duration = 5})
+    game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "Auto Rejoin Enabled!", Duration = 5})
     game:GetService("GuiService").ErrorMessageChanged:Connect(function()
         syn.queue_on_teleport([[game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)]])
         game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
