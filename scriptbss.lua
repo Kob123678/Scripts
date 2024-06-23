c9e59d581c89e17e7defc7e96cb1e9f7986f94e6
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-23 19:14:06 +0700

    Update scriptbss.lua

--- scriptbss.lua
+++ scriptbss.lua
@@ -1,15 +1,23 @@
-_G.Autojoin, _G.AntiAfk, _G.AutoRemovePortal, _G.AutoRejoin = _G.Autojoin or false, _G.AntiAfk or true, _G.AutoRemovePortal or true, _G.AutoRejoin or true
+_G.Autojoin = _G.Autojoin or false
+_G.AntiAfk = _G.AntiAfk or true
+_G.AutoRemovePortal = _G.AutoRemovePortal or true
+_G.AutoRejoin = _G.AutoRejoin or true
 
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local function promptTeleport()
-    local p, ui = game.Players.LocalPlayer, Instance.new("ScreenGui", p:WaitForChild("PlayerGui"))
-    local frame, label = Instance.new("Frame", ui), Instance.new("TextLabel", frame)
-    local yesButton, noButton = Instance.new("TextButton", frame), Instance.new("TextButton", frame)
+    local p = game.Players.LocalPlayer
+    local ui = Instance.new("ScreenGui", p:WaitForChild("PlayerGui"))
+    local frame = Instance.new("Frame", ui)
     frame.Size, frame.Position, frame.BackgroundTransparency = UDim2.new(0.3, 0, 0.2, 0), UDim2.new(0.35, 0, 0.4, 0), 0.5
+
+    local label = Instance.new("TextLabel", frame)
     label.Text, label.Size, label.Position, label.TextScaled, label.BackgroundTransparency = "Do you want to join Bee Swarm Simulator?", UDim2.new(1, 0, 0.5, 0), UDim2.new(0, 0, 0, 0), true, 1
+
+    local yesButton, noButton = Instance.new("TextButton", frame), Instance.new("TextButton", frame)
     yesButton.Text, yesButton.Size, yesButton.Position, yesButton.TextScaled = "Yes", UDim2.new(0.4, 0, 0.3, 0), UDim2.new(0.1, 0, 0.6, 0), true
     noButton.Text, noButton.Size, noButton.Position, noButton.TextScaled = "No", UDim2.new(0.4, 0, 0.3, 0), UDim2.new(0.5, 0, 0.6, 0), true
+
     yesButton.MouseButton1Click:Connect(function() game:GetService("TeleportService"):Teleport(1537690962, p) end)
     noButton.MouseButton1Click:Connect(function() ui:Destroy() end)
 end
@@ -21,10 +29,16 @@ if game.PlaceId ~= 1537690962 then
         promptTeleport()
     end
 else
-    if _G.AutoRemovePortal then workspace.RetroEvent.RetroChallengePortal.Trigger:Destroy() end
-    if _G.AntiAfk then
-        game.Players.LocalPlayer.Idled:connect(function() game:service('VirtualUser'):CaptureController() game:service('VirtualUser'):ClickButton2(Vector2.new()) end)
+    
+    if _G.AutoRemovePortal then
+        workspace.RetroEvent.RetroChallengePortal.Trigger:Destroy()
     end
+    
+    if _G.AntiAfk then
+        local vu = game:service('VirtualUser')
+        game.Players.LocalPlayer.Idled:connect(function() vu:CaptureController() vu:ClickButton2(Vector2.new()) end)
+    end
+    
     if _G.AutoRejoin then
         game.GuiService.ErrorMessageChanged:Connect(function()
             wait(0.1)
@@ -32,5 +46,6 @@ else
             game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
         end)
     end
+
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/BeeSwarmSimulator.lua"))()
 end
