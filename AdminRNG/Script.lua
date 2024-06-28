4b42a6d7f131de6ad7e0e255539bbf824eb29272
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-28 19:11:49 +0700

    Update Script.lua

--- AdminRNG/Script.lua
+++ AdminRNG/Script.lua
@@ -7,7 +7,7 @@ Credit:AddLabel("Made By SecondKob")
 
 local autoRollEnabled, antiAfkEnabled, autoRejoinEnabled = _G.AutoRoll or false, _G.AntiAfk or false, _G.AutoRejoin or false
 
-Main:AddToggle({Name = "Auto Roll", Default = autoRollEnabled, Callback = function(Value) _G.AutoRoll, autoRollEnabled = Value, Value end})
+Main:AddToggle({Name = "Auto Roll (Super Fast + No Cutscene)", Default = autoRollEnabled, Callback = function(Value) _G.AutoRoll, autoRollEnabled = Value, Value end})
 Main:AddToggle({Name = "Anti-AFK", Default = antiAfkEnabled, Callback = function(Value) _G.AntiAfk, antiAfkEnabled = Value, Value if Value then game.Players.LocalPlayer.Idled:Connect(function() if antiAfkEnabled then game:GetService("VirtualUser"):ClickButton2(Vector2.new()) end end) end end})
 Main:AddToggle({Name = "Auto Rejoin", Default = autoRejoinEnabled, Callback = function(Value) _G.AutoRejoin, autoRejoinEnabled = Value, Value if Value then game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child) if child.Name == "ErrorPrompt" then game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer) end end) end end})
 
