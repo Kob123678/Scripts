384e6c6b0da6095870633900ad8b0e14e62b6a23
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-06-29 20:36:54 +0700

    Update AutoRejoin.lua

--- AutoRejoin.lua
+++ AutoRejoin.lua
@@ -4,8 +4,8 @@ if not _G.AutoRejoin then
     _G.AutoRejoin = true
     game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "Auto Rejoin Enabled!", Duration = 5})
     game:GetService("GuiService").ErrorMessageChanged:Connect(function()
-        syn.queue_on_teleport([[game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)]])
-        game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+        wait(0.1) -- Wait a short delay to ensure the rejoin works properly
+        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
     end)
 else
     game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "You Already Executed!", Duration = 5})
