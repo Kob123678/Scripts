2e69524efba81f069ff1d4aaf028c43b09d53d63
Kob123678 <97777889+Kob123678@users.noreply.github.com>
2024-08-23 18:25:48 +0700

    Update FloodEscapeClassic.lua

--- FloodEscapeClassic.lua
+++ FloodEscapeClassic.lua
@@ -1,64 +1,47 @@
+-- From Tomato Hub
 repeat wait() until game:IsLoaded()
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ttwizz/Roblox/master/Orion.lua", true))()
-local Window = OrionLib:MakeWindow({Name = "Flood Escape"})
-local Tab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998"})
-local Section = Tab:AddSection({Name = "Auto Farm by Tomato Hub"})
 
-for _, v in ipairs({"Auto Farm", "Notifications"}) do
-    Tab:AddToggle({
-        Name = v,
-        Default = false,
-        Callback = function(val)
-            getgenv()[v:gsub(" ", ""):lower()] = val
-        end
-    })
+getgenv().autofarm, getgenv().Debug = true, true
+
+local lastdebug, LP, wait = tick(), game:GetService("Players").LocalPlayer, task.wait
+local gameModes, winsGained = {"Easy", "Medium", "Hard"}, 0
+
+local function debugprint(...) 
+    if Debug then warn(..., tick() - lastdebug) end
+    lastdebug = tick()
 end
-
-getgenv().autofarm, getgenv().notifications = false, false
-
-local LP, RunService, wait = game:GetService("Players").LocalPlayer, game:GetService("RunService"), task.wait
-local winsGained, gameModes = 0, {"Easy", "Medium", "Hard"}
+debugprint('Game Loaded')
 
 local function getThing(mode, thing)
     local mode = workspace[mode]
-    if thing:lower() == "entry" then return mode.Entry.LiftEntry end
-    if thing:lower() == "info" then return mode.Info end
+    if thing == "entry" then return mode.Entry.LiftEntry end
+    if thing == "info" then return mode.Info end
     return mode:WaitForChild('Main'):WaitForChild('Exit')
 end
 
-local function getRoot() return (LP.Character or LP.CharacterAdded:Wait()) and LP.Character:WaitForChild("HumanoidRootPart") end
-local function touchPart(part) local root = getRoot() firetouchinterest(root, part, 0) firetouchinterest(root, part, 1) end
-
-local function notify(content)
-    if getgenv().notifications then
-        OrionLib:MakeNotification({
-            Name = "Notification!",
-            Content = content,
-            Image = "rbxassetid://4483345998",
-            Time = 5
-        })
-    end
+local function touchPart(part)
+    local root = LP.Character and LP.Character:WaitForChild("HumanoidRootPart") or LP.CharacterAdded:Wait():WaitForChild("HumanoidRootPart")
+    firetouchinterest(root, part, 0)
+    firetouchinterest(root, part, 1)
 end
 
 local function playGame(mode)
+    debugprint('Entering Lift..')
     touchPart(getThing(mode, 'entry'))
-
     repeat wait() until getThing(mode, 'info').Value == "Game is Ready!"
-
-    for _ = 1, 3 do getThing(mode, 'info').Changed:wait() end
-
+    for i = 1, 3 do getThing(mode, 'info').Changed:Wait() end
+    debugprint('Touching Exit..')
     repeat wait() touchPart(getThing(mode, 'exit')) until LP.Ingame.Value == 0
     winsGained += 1
-
-    notify("Gained " .. winsGained .. " wins so far!")
 end
 
 while wait() do
-    if getgenv().autofarm and LP.Ingame.Value == 0 and LP.Waiting.Value == 0 then
+    if autofarm and LP.Ingame.Value == 0 and LP.Waiting.Value == 0 then
         for _, mode in ipairs(gameModes) do
-            local infoval = getThing(mode, 'info').Value
-            if infoval == "Game is Ready!" or infoval:lower():match('intermission') then
+            if getThing(mode, 'info').Value:lower():match('game is ready!|intermission') then
                 playGame(mode)
+                print("Gained", winsGained, "wins so far!")
+                break
             end
         end
     end
